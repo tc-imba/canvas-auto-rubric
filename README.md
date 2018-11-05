9d7ca9e0cd2a383f0e0512bdfe93b35555b045fd
tc-imba <liuyh615@sjtu.edu.cn>
2018-11-05 16:40:35 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Canvas Auto Rubric
+
+## Usage
+
+Run `data.js`
+
+## Plan
+
++ Add python support
++ Build it into a chrome plugin
---
+++ generate.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const crypto = require('crypto');
+const hash = crypto.createHash('sha256');
+
+const buffer = fs.readFileSync('ve281.csv');
+const data = buffer.toString().split('\n');
+let map = {};
+for (let i in data) {
+  if (data[i].length < 1) continue;
+  const a = data[i].split(',');
+  const student_id = a.shift();
+  map[student_id] = a;
+  // console.log(student_id, a);
+}
+// console.log(data);
+
+// console.log(map);
+try {
+  fs.mkdirSync('build');
+} catch (e) {
+}
+
+const map_str = JSON.stringify(map);
+hash.update(map_str);
+const data_var_name = `window._canvas_auto_rubric_data_${hash.digest('hex').
+    substring(0, 6)}`;
+fs.writeFileSync('build/index.js', [
+  `${data_var_name} = ${map_str};`,
+  `(`,
+  fs.readFileSync('template.js'),
+  `)(${data_var_name});`
+].join('\n'));
+
+console.log('successfully generated build/index.js');
+
