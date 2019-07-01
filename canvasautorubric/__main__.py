8062ca64bb49589db890fffe5e4fceb194bf87fa
tc-imba <liuyh615@sjtu.edu.cn>
2019-07-02 03:35:57 +0800

    add header

--- canvasautorubric/__main__.py
+++ canvasautorubric/__main__.py
@@ -9,14 +9,16 @@ def get_version():
     return pbr.version.VersionInfo('canvasautorubric')
 
 
-def generate_rubric_assessment(rubric_criteria, grades):
+def generate_rubric_assessment(rubric_criteria, rubric_description, grades):
     rubric_assessment = {}
     for i in range(len(rubric_criteria)):
         rubric_assessment[rubric_criteria[i]] = {'points': grades[i]}
+        if rubric_description:
+            rubric_assessment[rubric_criteria[i]]['comments'] = rubric_description[i]
     return rubric_assessment
 
 
-def update_grade(assignment, rubric_criteria, uid, grade, grades):
+def update_grade(assignment, uid, grade, grades, rubric_criteria, rubric_description):
     # print(uid, grade, grades)
     submission = assignment.get_submission(uid)
     data = {
@@ -25,7 +27,10 @@ def update_grade(assignment, rubric_criteria, uid, grade, grades):
         },
     }
     if rubric_criteria:
-        data['rubric_assessment'] = generate_rubric_assessment(rubric_criteria, grades)
+        data['rubric_assessment'] = generate_rubric_assessment(grades=grades,
+                                                               rubric_criteria=rubric_criteria,
+                                                               rubric_description=rubric_description)
+    # print(data)
     submission.edit(**data)
     print('Updated:', uid, grade, grades)
     return uid
@@ -39,9 +44,10 @@ def update_grade(assignment, rubric_criteria, uid, grade, grades):
 @click.option('-r', '--rubric-id', help='The Rubric ID of the target.')
 @click.option('-i', '--input', required=True, type=click.File(), help='CSV file with grades.')
 @click.option('--no-sum', is_flag=True, help='Use the last row of the grade file as the total grade.')
+@click.option('--header', is_flag=True, help='Use the first row of the grade file as description.')
 @click.help_option('-h', '--help')
 @click.version_option(version=get_version())
-def main(api_url, api_key, course_id, assignment_id, rubric_id, input, no_sum):
+def main(api_url, api_key, course_id, assignment_id, rubric_id, input, no_sum, header):
     canvas = Canvas(api_url, api_key)
     course = canvas.get_course(course_id)
     print('Course:', course)
@@ -66,14 +72,20 @@ def main(api_url, api_key, course_id, assignment_id, rubric_id, input, no_sum):
             rubric_criteria = None
 
     reader = csv.reader(input)
+    rubric_description = None
     for row in reader:
+        if header:
+            rubric_description = row[1:]
+            header = False
+            continue
         uid = row[0]
         grades = row[1:]
         if no_sum:
             grade = grades[-1]
         else:
             grade = sum(map(float, grades))
-        update_grade(assignment, rubric_criteria, uid, grade, grades)
+        update_grade(assignment=assignment, uid=uid, grade=grade, grades=grades,
+                     rubric_criteria=rubric_criteria, rubric_description=rubric_description)
 
 
 if __name__ == '__main__':
