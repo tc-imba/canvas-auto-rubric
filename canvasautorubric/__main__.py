b2dbe122bce23ee1f7e7d245a501845c0a12e834
tc-imba <liuyh615@sjtu.edu.cn>
2019-07-16 13:38:45 +0800

    fix a bug on modified rubric_assessment

--- canvasautorubric/__main__.py
+++ canvasautorubric/__main__.py
@@ -50,7 +50,8 @@ def update_grade(assignment, uid, grade, grades, rubric_criteria, rubric_descrip
                                                                rubric_criteria=rubric_criteria,
                                                                rubric_description=rubric_description)
     # print(data)
-    if rubric_assessment_is_modified(submission.rubric_assessment, data['rubric_assessment']):
+    if 'rubric_assessment' not in submission or \
+            rubric_assessment_is_modified(submission.rubric_assessment, data['rubric_assessment']):
         submission.edit(**data)
         print('Updated:', uid, grade, grades)
     else:
